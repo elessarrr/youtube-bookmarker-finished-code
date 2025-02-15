b4fb0ad7e026f782e1a717064e966f6b1c8440e9
Bhavesh R <bapuis@gmail.com>
2025-02-15 18:29:38 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 // Listen for tab updates
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, tab) => {    // Listen for an update to tabs
   // Only trigger for YouTube watch pages
   if (tab.url && tab.url.includes("youtube.com/watch")) {
     // Parse URL parameters
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
+    const queryParameters = tab.url.split("?")[1];            // unique id for each video
+    const urlParameters = new URLSearchParams(queryParameters); //just an interface to work with URLs
     
-    // Send video ID to content script
+    // Send video ID to content script "hey a new video has loaded; this is the ID"
     chrome.tabs.sendMessage(tabId, {
       type: "NEW",
       videoId: urlParameters.get("v"), // Extract YouTube video ID
